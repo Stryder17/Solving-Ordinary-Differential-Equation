90b16a3e06e05f9ac8473a773bc5ff653d3a93ef
Manjul Regmi <manjulregmi@gmail.com>
2023-11-04 18:17:12 -0500

    Integrate Lyapunov function into one script

--- Lyapunov.m
+++
@@ -1,7 +0,0 @@
-% Manjul Regmi
-
-function SDotv = Lyapunov(t,Sv,A,Q)
-    S = [Sv(1) Sv(2); Sv(2) Sv(3)];
-    SDot = A'*S+S*A+Q;
-    SDotv = [SDot(1,1); SDot(1,2); SDot(2,2)];
-end
--- solve_diff_equn.m
+++ solve_diff_equn.m
@@ -44,3 +44,8 @@ for i = 1:length(t)
     S(2,2,i) = Sv(i,3);
 end
 
+function SDotv = Lyapunov(t,Sv,A,Q)
+    S = [Sv(1) Sv(2); Sv(2) Sv(3)];
+    SDot = A'*S+S*A+Q;
+    SDotv = [SDot(1,1); SDot(1,2); SDot(2,2)];
+end
